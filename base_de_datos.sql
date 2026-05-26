b52299add003cae0b0238fb251321673eb61ff26
Eliezer Ariel Romero Medina <3850308@incb.edu.sv>
2026-05-26 09:15:42 -0600

    Documentadas consultas avanzadas y reportes con INNER JOIN

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -33,4 +33,18 @@ INSERT INTO categorias (nombre_categoria) VALUES
 enteros)
 INSERT INTO productos (nombre_producto, categoria_id, stock, precio) VALUES
 ('Laptop Dell Inspiron 15', 1, 15, 720.00),
-('Mouse Inalámbrico Logitech', 2, 25, 12.00);
+('Mouse Inalámbrico Logitech', 2, 25, 12.00);  
+
+-- guia 11 REPORTE RELACIONADOS AVANZADOS
+--1 REPORTE GENERAL DE INVENTARIO
+SELECT p.id , p.nombre_producto,c.nombre_categoria, p.stock, p.precio
+FROM productos p
+INNER JOIN categorias c 
+ON p.categoria_id=c.id;
+
+--2 REPORTE FILTRADO POR DEPARTAMENTO
+SELECT p.id , p.nombre_producto,c.nombre_categoria, p.stock, p.precio
+FROM productos p
+INNER JOIN categorias c 
+ON p.categoria_id=c.id
+WHERE c.nombre_categoria = "Accesorios";
